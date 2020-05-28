5b16018e9abc758a9d85e821cb795a79f5bf4184
zfh <fuhongzuo@gmail.com>
2020-05-28 22:53:47 +0800

    获取最大的Seq

--- internal/logic/service/seq.go
+++ internal/logic/service/seq.go
@@ -1,6 +1,9 @@
 package service
 
-import "go-IM/internal/logic/dao"
+import (
+	"go-IM/internal/logic/dao"
+	"go-IM/internal/logic/model"
+)
 
 type seqService struct{}
 
@@ -8,10 +11,10 @@ var SeqService = new(seqService)
 
 // 获取下一个序列号
 func (*seqService) GetUserNext(appId, userId int64) (int64, error) {
-	return dao.MessageDao.GetMaxByObjectId(appId, 1, userId)
+	return dao.MessageDao.GetMaxByObjectId(appId, model.MessageObjectTypeUser, userId)
 }
 
 // 获取下一个序列号
 func (*seqService) GetGroupNext(appId, groupId int64) (int64, error) {
-	return dao.MessageDao.GetMaxByObjectId(appId, 2, groupId)
+	return dao.MessageDao.GetMaxByObjectId(appId, model.MessageObjectTypeGroup, groupId)
 }
